0fe37e67aa10fbdc6f51d48487ee54047515098b
tiash007 <jktiash123@gmail.com>
2020-11-10 10:05:50 +0600

    first

--- lab_exam/php/logincheck.php
+++ lab_exam/php/logincheck.php
@@ -1,13 +1,16 @@
 <?php
 if (isset($_POST['submit'])) {
-	# code...
+	
 
 
 	$myfile = fopen('users.txt', 'r');
 	$data = fread($myfile, filesize('test.txt'));
 	$data = fgets($myfile);
 	echo $data;
-
+$words = explode("|", $data);
+foreach ($words as $word) {
+	echo $word;
+}
 	}
 
 
