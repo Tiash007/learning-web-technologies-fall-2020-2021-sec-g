539284b149f4659a68ac5c415c87cd988f75dd3f
tiash007 <jktiash123@gmail.com>
2020-12-01 09:12:33 +0600

    commit at 8:05 am

--- FINAL_LAB_TASK_1.2_PHP/crud.php
+++ FINAL_LAB_TASK_1.2_PHP/crud.php
@@ -1,5 +1,7 @@
 <?php
-
+$conn = mysqli_connect('localhost', 'root', '', 'webtech');
+	$sql = 'select * from esheet';
+	$result = mysqli_query($conn, $sql);
 ?>
 
 <!DOCTYPE html>
@@ -8,6 +10,26 @@
 	<title></title>
 </head>
 <body>
+<table border="1"> 
+		<tr>
+			<td>ID</td>
+			<td>USERNAME</td>
+			<td>PASSWORD</td>
+			<td>EMAIL</td>
+			<td>TYPE</td>
+		</tr>
 
+<?php while($data = mysqli_fetch_assoc($result)){ ?>
+
+		<tr>
+			<td><?=$data['id']?></td>
+			<td><?=$data['username']?></td>
+			<td><?=$data['password']?></td>
+			<td><?=$data['email']?></td>
+			<td><?=$data['type']?></td>
+		</tr>
+<?php } ?>
+
+</table>
 </body>
 </html>
